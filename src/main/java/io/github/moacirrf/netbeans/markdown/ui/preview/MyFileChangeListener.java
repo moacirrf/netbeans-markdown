27e58ec4f35163389de82f53aba567bcd9496a76
Moacir da Roza Flores <moacirrf@gmail.com>
2022-09-13 22:52:12 -0300

    Listener that verify if file was saved on disc

---
+++ src/main/java/io/github/moacirrf/netbeans/markdown/ui/preview/MyFileChangeListener.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright (C) 2022 Moacir da Roza Flores <moacirrf@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package io.github.moacirrf.netbeans.markdown.ui.preview;
+
+import org.openide.filesystems.FileAttributeEvent;
+import org.openide.filesystems.FileChangeListener;
+import org.openide.filesystems.FileEvent;
+import org.openide.filesystems.FileRenameEvent;
+
+/**
+ *
+ * @author Moacir da Roza Flores <moacirrf@gmail.com>
+ */
+public class MyFileChangeListener implements FileChangeListener {
+
+    private final MarkdownPreviewScrollPane markdownPreviewScrollPane;
+
+    public MyFileChangeListener(MarkdownPreviewScrollPane pane) {
+        this.markdownPreviewScrollPane = pane;
+    }
+
+    @Override
+    public void fileFolderCreated(FileEvent fe) {
+    }
+
+    @Override
+    public void fileDataCreated(FileEvent fe) {
+    }
+
+    @Override
+    public void fileChanged(FileEvent fe) {
+        markdownPreviewScrollPane.fillEditorPane();
+    }
+
+    @Override
+    public void fileDeleted(FileEvent fe) {
+    }
+
+    @Override
+    public void fileRenamed(FileRenameEvent fre) {
+    }
+
+    @Override
+    public void fileAttributeChanged(FileAttributeEvent fae) {
+    }
+
+}
